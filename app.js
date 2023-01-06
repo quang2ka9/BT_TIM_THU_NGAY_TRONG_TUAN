4ce2fab9dc41b522148d3310a8b599ffa7652024
quang <ltb01628645546@gmail.com>
2023-01-06 21:26:11 +0700

    langg

---
+++ app.js
@@ -0,0 +1,33 @@
+let day_name = ["chủ nhật", "thứ hai", "thứ ba", "thứ tư", "thứ năm", "thứ sáu", "thứ bảy"];
+
+let date = new Date();
+
+
+let current_day = date.getDay();
+
+
+
+switch (current_day) {
+    case 0:
+        day_name = "Chủ nhật";
+        break;
+    case 1:
+        day_name = "Thứ hai";
+        break;
+    case 2:
+        day_name = "Thứ ba";
+        break;
+    case 3:
+        day_name = "Thứ tư";
+        break;
+    case 4:
+        day_name = "Thứ năm";
+        break;
+    case 5:
+        day_name = "Thứ sáu";
+        break;
+    case 6:
+        day_name = "Thứ bảy";
+}
+
+console.log(day_name);
